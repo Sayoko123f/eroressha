b6822f46fc8281d4eca6508108590bb7ec35aca4
Sayoko123f <63059711+Sayoko123f@users.noreply.github.com>
2022-02-06 17:16:41 +0800

    Create center-center-modal.vue

---
+++ src/components/center-center-modal.vue
@@ -0,0 +1,26 @@
+<template>
+  <div
+    class="absolute inset-0 flex items-center justify-center bg-black bg-opacity-40"
+    :style="{ zIndex: z }"
+    @click.self="close"
+  >
+    <slot :close="close"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  emits: ["close"],
+  props: {
+    z: {
+      type: Number,
+      default: 30,
+    },
+  },
+  methods: {
+    close() {
+      this.$emit("close");
+    },
+  },
+};
+</script>
